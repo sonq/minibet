208925d54284a51c33c7f4648f32f8964e5deba0
sonq <soner.kurceren@gmail.com>
2018-07-08 15:58:32 +0300

    finals2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update,:show]
-  before_action :require_user
+
 
   def new
 
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require_relative 'boot'
 
 require "rails"
@@ -20,6 +21,7 @@ module Taqtaqbet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
