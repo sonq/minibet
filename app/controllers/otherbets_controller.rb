b8160359135cfbbabe2bd313db634d71ca4d5bef
sonq <soner.kurceren@gmail.com>
2018-08-25 18:26:21 +0300

    other bets2

--- app/controllers/otherbets_controller.rb
+++ app/controllers/otherbets_controller.rb
@@ -5,7 +5,7 @@ class OtherbetsController < ApplicationController
   before_action :require_user
     
   def index
-    @otherbets = Otherbet.order('username DESC')
+    @otherbets = Otherbet.order('username ASC')
   end
  
 
