36f961be3ddbcee70f9095925861be7391961c40
sonq <soner.kurceren@gmail.com>
2018-08-31 09:58:39 +0300

    other bets list update

--- app/controllers/otherbets_controller.rb
+++ app/controllers/otherbets_controller.rb
@@ -5,7 +5,7 @@ class OtherbetsController < ApplicationController
   before_action :require_user
     
   def index
-    @otherbets = Otherbet.order(due: :desc, username: :asc)
+    @otherbets = Otherbet.order(due: :desc, description: :asc)
   end
  
 
