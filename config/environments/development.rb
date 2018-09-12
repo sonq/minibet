5d4020f106e0f605fa1684e606ec08fa6da69e67
sonq <soner.kurceren@gmail.com>
2018-09-12 17:07:38 +0300

    Mailer Optimization

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-    address: "smtp.example.com",
+    address: "smtp.gmail.com",
     port: 587,
     authentication: "plain",
     user_name: 'taqtaqbet@gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-    address: "smtp.example.com",
+    address: "smtp.gmail.com",
     port: 587,
     authentication: "plain",
     user_name: 'taqtaqbet@gmail.com',
